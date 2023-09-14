6948498d1b66da276c2d539550befd8fac4948e3
Nicolas <nicolasandrade.de.freitas@gmail.com>
2023-09-14 00:26:49 -0300

    feat:  generate transcription

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello world!";
+app.register(fastifyCors, {
+  origin: "*",
 });
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
 app
   .listen({
