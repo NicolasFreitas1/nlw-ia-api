622c99b2e7c50da106892222293495cefc41b7dd
Nicolas <nicolasandrade.de.freitas@gmail.com>
2023-09-16 13:21:52 -0300

    feat:  generate stream

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { createReadStream } from "node:fs";
 import { z } from "zod";
+import { streamToResponse, OpenAIStream } from "ai";
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -40,10 +40,16 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
           content: promptMessage,
         },
       ],
+      stream: true,
     });
 
-    return {
-      response,
-    };
+    const stream = OpenAIStream(response);
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      },
+    });
   });
 }
